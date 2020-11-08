9d59031a84398372b3e2fb5fab4910c48915c4c0
abhishek-548 <72663197+abhishek-548@users.noreply.github.com>
2020-11-08 19:27:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 
+import json
 
 app = Flask(__name__)
 # signup or home page
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-  return render_template("index.html")
+  if request.method=="POST":
+    name = request.form.get("name")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    data = {
+      "name":name,
+      "email":email,
+      "password":password,
+    }
+    f = open("data.txt","a")
+    f.write(json.dumps(data))
+    f.write("\n")
+    f.close()
+    return render_template("tologin.html")
+  
+  return render_template("signup.html")
+
+
+#login page
+@app.route("/login", methods=["GET","POST"])
+def login():
+  if request.method=="POST":
+    email = request.form.get("email")
+    password = request.form.get("password")
+    f = open("data.txt","r")
+    for i in f:
+      j = json.loads(i)
+      if j["email"]==email and j["password"]==password:
+        return render_template("index.html")
+    else:
+      return render_template("signup.html")
+  else:
+    return render_template("login.html")
 
 #covidpage
 @app.route("/covidpage")
