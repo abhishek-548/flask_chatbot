ec5d5ccf3f2334f019f4a436cd8a4db265a12bec
abhishek-548 <72663197+abhishek-548@users.noreply.github.com>
2020-11-16 12:41:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,11 @@ def chatbotworld():
   return render_template("chatbotworld.html")
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+
 
 
 
